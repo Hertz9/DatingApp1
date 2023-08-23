be72eec0cf32a20844bcba6f68bb7589cca8dc4e
Hrishi <szinjarde@gmail.com>
2023-08-23 22:48:21 +0530

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //use prop for creating properties
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+} 
